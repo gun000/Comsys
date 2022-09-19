99831beb1a3f084c0d472e4209efc3a982e4c4dc
gun000 <88178216+gun000@users.noreply.github.com>
2022-09-19 16:43:30 +0900

    Add files via upload

---
+++ ftpupload.ps1
@@ -0,0 +1,9 @@
+﻿mkdir C:\temp
+'open 192.168.0.40 21
+user "Administrator" "Sentinel1!"
+lcd C:\temp
+put FTPTEST.zip' | Out-File -FilePath 'C:\temp\.\script.txt' #FTP 접속 파일 생성
+
+cd C:\temp
+Compress-Archive -Path "C:\Users\Gunyoung\Desktop\FTPTEST" "C:\temp\FTPTEST.zip" -CompressionLevel optimal # 압축
+ftp -v -n -i -d -s:"C:\temp\script.txt" #FTP
